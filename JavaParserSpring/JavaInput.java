e26d494453822da2c3a53872b037ca7f8d741ffe
Vaishnavi Galgali <vaishnavigalgali109@gmail.com>
2015-10-22 01:03:46 -0700

    Adding implementation for uses & constructor

--- JavaParserSpring/JavaInput.java
+++ JavaParserSpring/JavaInput.java
@@ -1,52 +1,145 @@
- 
+import java.util.*;
 
-import java.util.Collection;
+public class ConcreteObserver implements Observer {
+ 
+	protected String observerState;
+	protected ConcreteSubject subject;
 
-public class A extends B implements E,F{
- 
-	private int x;
-	 
-	private int[] y;
-	 
-	private B b;
-	 
-	private C c;
-	 
-	private D d;
-	private Collection<D> d;
+    public ConcreteObserver( ConcreteSubject theSubject )
+    {
+        this.subject = theSubject ;
+    }
+    
+	public void update() {
+	    // do nothing
+	}
 
-	 
-	public void test(int a,int b){}
-	private int abc(){}
-	protected int xyz(){}
-	abstract int xyz(){}
-	static int xyz(){}
-}
- 
- 
-
-public class B {
- 
-	private A a;
+    public void showState()
+    {
+        System.out.println( "Observer: " + this.getClass().getName() + " = " + observerState );
+    }
 	 
 }
  
-  
+                    
 
-public class C {
+
+public class ConcreteSubject implements Subject {
  
-	private A a;
+	private String subjectState;
+	 
+	private Collection<Observer> observers = new ArrayList<Observer>() ;
+	 
+	public String getState() {
+		return subjectState ;
+	}
+	 
+	public void setState(String status) {
+	    subjectState = status ;
+        notifyObservers();
+	}
+
+	public void attach(Observer obj) {
+	    observers.add(obj) ;
+	}
+
+	public void detach(Observer obj) {
+        observers.remove(obj) ;
+	}
+
+	public void notifyObservers() {
+        for (Observer obj  : observers)
+        {
+            obj.update();
+        }
+	}
+
+    public void showState()
+    {
+        System.out.println( "Subject: " + this.getClass().getName() + " = " + subjectState );
+    }
 	 
 }
  
+
+public interface Observer {
+ 
+	public abstract void update();
+}
  
 
-
-
-
-public class D {
+public class Optimist extends ConcreteObserver {
  
-	private A a;
+    public Optimist( ConcreteSubject sub )
+    {
+        super( sub ) ;
+    }
+    
+	public void update() {
+	    if ( subject.getState().equalsIgnoreCase("The Price of gas is at $5.00/gal")      )
+        {
+             observerState = "Great! It's time to go green." ;
+        }
+        else if ( subject.getState().equalsIgnoreCase( "The New iPad is out today" ) )
+        {
+            observerState = "Apple, take my money!" ;
+        }
+        else
+        {
+            observerState = ":)" ;
+        }
+	}
 	 
 }
  
+
+
+public class Pessimist extends ConcreteObserver {
+
+    public Pessimist( ConcreteSubject sub )
+    {
+        super( sub ) ;
+    }
+
+    public void update() {
+        if ( subject.getState().equalsIgnoreCase("The Price of gas is at $5.00/gal") )
+        {
+            observerState = "This is the beginning of the end of the world!" ;
+        }
+        else if ( subject.getState().equalsIgnoreCase( "The New iPad is out today" ) )
+        {
+            observerState = "Not another iPad!"  ;
+        }
+        else
+        {
+            observerState = ":(" ;
+        }
+	}
+	 
+}
+ 
+
+public interface Subject {
+ 
+	public void attach(Observer obj);
+	public void detach(Observer obj);
+	public void notifyObservers();
+}
+ 
+
+public class TheEconomy extends ConcreteSubject {
+
+    public TheEconomy()
+    {
+        super.setState("The Price of gas is at $5.00/gal");
+    }
+	 
+}
+
+/*
+
+The Price of gas is at $5.00/gal
+The New iPad is out today
+
+*/
+
--- JavaParserSpring/src/main/java/Parser.java
+++ JavaParserSpring/src/main/java/Parser.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -19,6 +20,7 @@ import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
@@ -33,7 +35,10 @@ import net.sourceforge.plantuml.SourceStringReader;
 public class Parser {
 	public static HashMap<String, String> dependencyMap = new HashMap<String, String>();
 	public static String data ="@startuml\n";
-	
+	public static ArrayList multiplicity = new ArrayList();
+	public static ArrayList OneToOneMultiplicity = new ArrayList();
+	public static ArrayList Uses = new ArrayList();
+	public static ArrayList Interfaces = new ArrayList();
 	public static void main(String[] args) throws ParseException, IOException {
 		// TODO Auto-generated method stub
 		FileInputStream in = new FileInputStream("JavaInput.java");
@@ -46,18 +51,54 @@ public class Parser {
 			in.close();
 		}
 		new MethodVisitor().visit(cu, null);
-		System.out.println("Out of visist");
+		
+		
 		 Set setDependency = dependencyMap.entrySet();
 		 Iterator iterator = setDependency.iterator();
 		 while(iterator.hasNext()) {
 	          Map.Entry mentry = (Map.Entry)iterator.next();
-	          if(mentry.getKey().toString().contains("<")||mentry.getValue().toString().contains("<"))
-	        	  System.out.println(mentry.getKey().toString());
-	          data+="\nclass "+mentry.getKey().toString()+" -- "+"class  "+mentry.getValue().toString()+"\n";
-	       //   System.out.println(mentry.getKey().toString() + "  "+mentry.getValue().toString());
+	          boolean state = data.contains("\nclass "+mentry.getKey().toString()+" -- "+"class  "+mentry.getValue().toString());
+	          boolean state1 = data.contains("\nclass "+mentry.getValue().toString()+" -- "+"class  "+mentry.getKey().toString());
+	          if(!data.contains("\nclass "+mentry.getKey().toString()+" -- "+"class  "+mentry.getValue().toString()) &&
+	        		  !data.contains("\nclass "+mentry.getValue().toString()+" -- "+"class  "+mentry.getKey().toString()))
+	          {
+	        	  System.out.println(mentry.getKey().toString()+" + "+mentry.getValue().toString());
+	        	  if(multiplicity.contains(mentry.getKey().toString()+"-"+mentry.getValue().toString()) &&
+	        			  !data.contains(mentry.getKey().toString()+'"'+"1"+'"'+" -- "+'"'+ "0..*"+'"'+" "+mentry.getValue().toString()+"\n"))
+	        		  data+=mentry.getKey().toString()+'"'+"1"+'"'+" -- "+'"'+ "0..*"+'"'+" "+mentry.getValue().toString()+"\n";
+	        	  else if(multiplicity.contains(mentry.getValue().toString()+"-"+mentry.getKey().toString())&&
+	        			  !data.contains(mentry.getValue().toString()+'"'+"1"+'"'+" -- "+'"'+ "0..*"+'"'+" "+mentry.getKey().toString()+"\n"))
+	        		  data+=mentry.getValue().toString()+" "+'"'+"1"+'"'+" -- "+'"'+ "0..*"+'"'+" "+mentry.getKey().toString()+"\n";
+	        	  else
+	              data+="\nclass "+mentry.getKey().toString()+" -- "+"class  "+mentry.getValue().toString()+"\n";
+	          }
+	       
 	      }
+		 /*
+		 for(int i=0;i<Uses.size();i++){
+			 
+			 String[] segments = Uses.get(i).toString().split("-", 2);
+			 if(!segments[0].equals("int") && !segments[0].equals("float") && !segments[0].equals("String") && !segments[0].equals("double") && !segments[0].equals("byte") 
+			&& !segments[0].equals("boolean") && !segments[0].equals("short") && !segments[0].equals("long") ){
+				data+="\n"+segments[0] + " <.. " +segments[1]+"\n";
+			 
+		 }
+		 for(int i=0;i<OneToOneMultiplicity.size();i++){
+			 
+			 String[] segments = OneToOneMultiplicity.get(i).toString().split("-", 2);
+			 data+=segments[0]+'"'+"1"+'"'+" - "+'"'+ "1"+'"'+" "+segments[1]+"\n";
+		 }
+		*/
+		 for(int i=0;i<Uses.size();i++){
+			 String[] segments = Uses.get(i).toString().split("-", 2);
+			 if(!segments[0].equals("int") && !segments[0].equals("float") && !segments[0].equals("String") && !segments[0].equals("double") && !segments[0].equals("byte") 
+					 && !segments[0].equals("boolean") && !segments[0].equals("short") && !segments[0].equals("long") ){
+				data+="\n"+segments[0] + " <.. " +segments[1]+"\n";
+		 }
+		}
 		data+="\n@enduml\n";
-		//System.out.println(data);
+		
+		System.out.println(data);
 		
 		SourceStringReader reader=new SourceStringReader(data);
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
@@ -74,9 +115,39 @@ public class Parser {
 
 		@Override
 		public void visit(ClassOrInterfaceDeclaration n, Object arg) {
-			
+			n.getAllContainedComments();
+		
 			String ClassName = n.getName();
-			
+			if(n.isInterface()){
+				data+="interface\t"+ClassName+"{"+"\n";
+				Interfaces.add(ClassName);
+				List<BodyDeclaration> members = n.getMembers();
+				if(members!=null){
+					for (BodyDeclaration member : members) {
+						 if(member instanceof MethodDeclaration)
+						{
+							MethodDeclaration method = (MethodDeclaration)member;	
+							List<Parameter> params = method.getParameters();
+							if(params==null)
+							data+=method.getName().toString()+" : "+method.getType().toString()+"\n";
+							else{
+							int modifier=method.getModifiers();
+							String modifierSymbol= GetModifier(modifier);
+							data+=modifierSymbol +" "+ method.getName().toString()+"(";
+							for(Parameter param : params){
+								data+=param.getId()+" : "+param.getType()+" ";
+								GetUsesRelation(param.getType().toString(),ClassName);
+								}
+							data+=")"+" : "+method.getType().toString()+"\n";
+							}
+						}
+					}
+					
+				}
+				data+="\n"+"}"+"";
+			}
+			else{
+				
 			List<ClassOrInterfaceType> extClasses = n.getExtends();
 			if(extClasses!=null)
 				for(ClassOrInterfaceType ext : extClasses){
@@ -87,7 +158,7 @@ public class Parser {
 			List<ClassOrInterfaceType> interfaces = n.getImplements();
 			if(interfaces!=null){
 				for(ClassOrInterfaceType infc : interfaces){
-					data+=infc.getName() +"()-"+ClassName+"\n";
+					data+=infc.getName() +"<|.."+ClassName+"\n";
 				}
 			}
 			data+="class "+ClassName+"{\n";
@@ -95,13 +166,28 @@ public class Parser {
 			if(members!=null)
 			{
 				for (BodyDeclaration member : members) {
+					if(member instanceof ConstructorDeclaration){
+						ConstructorDeclaration construct = (ConstructorDeclaration)member;
+						String modifierSymbol= GetModifier(construct.getModifiers());
+						data+=modifierSymbol+" "+construct.getName()+"(";
+						List<Parameter> ConstructParams = construct.getParameters();
+						if(ConstructParams!=null){
+							for(Parameter para : ConstructParams){
+								data+=para.getId()+" : "+para.getType()+" ";
+								GetUsesRelation(para.getType().toString(),ClassName);
+							}
+							
+						}
+						data+=")\n";
+						
+					}
 					if(member instanceof FieldDeclaration)
 					{
 
 						FieldDeclaration vars = (FieldDeclaration)member;
 						int abc = vars.getModifiers();
 						String type1 = vars.getType().toString();
-			            System.out.println(type1);
+			            //System.out.println(type1);
 						List<VariableDeclarator> variables = vars.getVariables();
 						
 						if(variables!=null){
@@ -128,15 +214,22 @@ public class Parser {
 						data+=modifierSymbol +" "+ method.getName().toString()+"(";
 						for(Parameter param : params){
 							data+=param.getId()+" : "+param.getType()+" ";
+							GetUsesRelation(param.getType().toString(),ClassName);
 							}
 						data+=")"+" : "+method.getType().toString()+"\n";
 						}
 					}
 				}
-				data+="}\n";
+				data+="\n}\n";
 			}
 		}
-
+		}
+		private void GetUsesRelation(String Type, String className) {
+			
+				Uses.add(Type+"-"+className);
+			
+			
+		}
 		private void GetVariableType(FieldDeclaration vars, String ClassName) {
 			if(vars.getType().toString().equals("int")||vars.getType().toString().equals("String")||
 					vars.getType().toString().equals("float")||vars.getType().toString().equals("double")||
@@ -157,11 +250,21 @@ public class Parser {
 			else{
 					if(vars.getType().toString().contains("<"))
 					{
+						int beginIndex=vars.getType().toString().indexOf("<");
+						int endIndex = vars.getType().toString().indexOf(">");
+						String CollectionClass = vars.getType().toString().substring(beginIndex+1, endIndex);
 						
+						dependencyMap.put(CollectionClass,ClassName);
+						data+=" : "+CollectionClass+"[*]"+"\n";
+						//String multi  = ClassName+" 1"+" - "+ " 0..* "+CollectionClass;
+						multiplicity.add(ClassName+"-"+CollectionClass);
 					}
+					else{
 					data+= " : "+vars.getType().toString()+"\n";
 					dependencyMap.put(vars.getType().toString(),ClassName);
-					System.out.println(vars.getType().toString() + "  "+ClassName);
+					String multi  = ClassName+" 1"+" - "+ " 1 "+vars.getType().toString();
+					OneToOneMultiplicity.add(ClassName+"-"+vars.getType().toString());
+					}
 				}
 	
 		}
